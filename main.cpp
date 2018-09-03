944ea2a1b1cdfba4e05466911d19bb3aafd7c03b
Ferai Ali <ferai.ali@egt-bg.com>
2018-09-03 18:34:52 +0300

    Add character class part 2

--- main.cpp
+++ main.cpp
@@ -2,9 +2,47 @@
 #include "SFML/Graphics.hpp"
 #include <Thor/Resources.hpp>
 #include <Box2D/Box2D.h>
+#include "AssetsParser/CharacterParser.h"
+#include "Core/Characters/Character.h"
 
 int main()
 {
+    CharacterParser cp;
+    cp.Parse();
+
+    std::map<std::string, std::vector<sf::Texture>> animTextures;
+    for(auto& p : cp.charactersKeys)
+    {
+        if((p[1] == std::string("attack")))
+        {
+            std::cout << p[2] << std::endl;
+        }
+    }
+
+    for(const auto& keys : cp.charactersKeys)
+    {
+        const auto& animId = keys[1];
+        const auto& path = keys[2];
+        auto& textureList = animTextures[animId];
+        textureList.push_back(sf::Texture());
+        textureList.back().loadFromFile(path);
+    }
+
+    core::Character knight;
+    for(const auto& p : animTextures)
+    {
+        const auto& animId = p.first;
+        const auto& textureList = p.second;
+        std::vector<sf::Sprite> sprites(textureList.size());
+        for(size_t i = 0; i < sprites.size(); i++)
+        {
+            sprites[i].setTexture(textureList[i]);
+        }
+
+        knight.AddAnimation(animId, sprites);
+    }
+    knight.SetScale({0.25f, 0.25f});
+
     sf::RenderWindow window(sf::VideoMode(1400, 1024), "FREEBIES");
     window.setFramerateLimit(60);
 
@@ -27,6 +65,12 @@ int main()
 
             }
         }
+        knight.Play("idle", 600ms, core::ReplayPolicy::Never);
+
+
+        knight.Update(15ms);
+        knight.Draw(window);
+
         window.display();
     }
 
